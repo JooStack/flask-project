8dac2de251b870264089a21d80ce4c94ca23544f
JooStack <joostack@gmail.com>
2022-05-14 01:50:55 +0300

    updated

--- blog.py
+++ blog.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index() :
-    return render_template("index.html")
+    articles = [
+        {"id":1,"title":"Deneme1","content":"Deneme1 içerik"},
+        {"id":2,"title":"Deneme2","content":"Deneme2 içerik"},
+        {"id":3,"title":"Deneme3","content":"Deneme3 içerik"}
+    ]
+    return render_template("index.html",answer="evet", articles = articles)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/article/<string")
 if __name__ == "__main__" :
     app.run(debug=True)
 
