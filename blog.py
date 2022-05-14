2a6ac433822e73cee7b06074bc4279268bdfb5bd
JooStack <joostack@gmail.com>
2022-05-14 10:57:38 +0300

    updated

--- blog.py
+++ blog.py
@@ -5,6 +5,16 @@ from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "myblog"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
+
+
 @app.route("/")
 def index() :
     articles = [
